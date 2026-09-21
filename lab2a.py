be874015cc9d6f9cc275c27bfb1bf177f477beb9
asadruddin <asadruddin@myseneca.ca>
2026-09-21 11:32:06 -0400

    Add author and date comments in lab2a.py
    Added author and date comments, and updated input handling.

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,21 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: ayaan sadruaddin
+# Date: 21 sep 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
+x=input('ENTER THE NUMBER:')
+print(type(x))
 
 # TO DO 1: Follow the instructions given in README.md file
+x=int(x)
+#check if x is greater then 6 or equal and print.
+if x >= 6: print('x is greater then 6!')
 
+#check if x is greater than or equal to 4 AND less than 12.
+if x >= 4 and x < 12: print("x is greater than or equal to 4 and less than 12!")
+  
 
 
 
