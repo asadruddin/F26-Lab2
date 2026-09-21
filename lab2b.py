05b87ac260d065be66c65e3ed50293abc4bf7614
asadruddin <asadruddin@myseneca.ca>
2026-09-21 11:54:24 -0400

    Add author info and implement user input logic
    Added author and date comments, and implemented user input for a 4-digit integer with conditional output.

--- lab2b.py
+++ lab2b.py
@@ -1,11 +1,14 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: ayaan sadruddin
+# Date: 21 september 2026
 # Purpose: Practice using if and else statments.
 # Usage: ./lab2b.py
-
+num = int(input("Enter a 4 digit integer: "))
 # TO DO 1:
 # Follow the instructions given in the README.md file.
-
+if num==1984:
+  print("George Orwell")
+else: 
+  print("Not quite right!")
