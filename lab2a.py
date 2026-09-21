4fe2a67f4666efa9dda13c92d432e681b9e1914a
asadruddin <asadruddin@myseneca.ca>
2026-09-21 11:22:39 -0400

    Update comments and fix variable type handling
    Added author and date comments, corrected variable case and type conversion.

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,14 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: ayaan sadruddin
+# Date: 21st sep 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
-
+x=input('enter the number:')
+print(type(X))
 # TO DO 1: Follow the instructions given in README.md file
-
+X=INT(X)
 
 
 
